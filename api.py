03fe7aba19ffb2398ac614cef6065836fdfe3129
marieemanuellemorin <62451810+marieemanuellemorin@users.noreply.github.com>
2020-03-21 14:12:44 -0400

    Update api.py
    fonction lister_partie

--- api.py
+++ api.py
@@ -1,1 +1,15 @@
+def lister_parties(idul):
 
+    import requests
+
+    url_base = 'https://python.gel.ulaval.ca/quoridor/api'
+
+    rep = requests.get(f'{url_base}/lister/', params={'idul': 'xxxxx'})
+    if rep.status_code == 200:
+        # la requête s'est déroulée normalement; décoder le JSON
+        
+        rep = rep.json()
+        print(rep)
+    
+    else:
+        print(f"Le GET sur {url_base+'lister'} a produit le code d'erreur {rep.status_code}.")
