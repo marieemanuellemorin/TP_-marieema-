59fff13f9176aa1d7d08a7cc045686ee8a7fcb1e
marieemanuellemorin <62451810+marieemanuellemorin@users.noreply.github.com>
2020-03-21 14:11:43 -0400

    Update quoridor.py
    fonction analyser_commande

--- quoridor.py
+++ quoridor.py
@@ -1,1 +1,28 @@
-#hello
+import argparse
+
+
+def analyser_commande():
+    python main.py nom_du_joueur
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'nom_du_joueur', nargs='1', dest='idul', help='IDUL du joueur'
+        )
+    parser.add_argument(
+        '-h','--help',
+        metavar='TYPE', dest='help' help="show this help message and exit"
+    )
+    parser.add_argument(
+        '-l', '--lister',
+        metavar='TYPE', dest='lister', help='lister les identifiants de vos 20 dernières parties'
+    )
+    return parser.parse_args()
+
+def afficher_damier_ascii(état_jeu):
+    position_idul = état_jeu["joueurs"][0]["pos"]
+    position_auto = état_jeu["joueurs"][1]["pos"]
+    murh = état_jeu["murs"]["horizontaux"]
+    murv = état_jeu["murs"]["verticaux"]
+    x = 20
+    y = 20
+
+    print(f'Légende: 1={état_jeu["joueurs"][0]["nom"]}, 2={état_jeu["joueurs"][1]["nom"]}'')
