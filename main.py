076ea07154f2176c4c2cd2ad2ebf13bc8e423315
marieemanuellemorin <62451810+marieemanuellemorin@users.noreply.github.com>
2020-03-24 16:11:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from quoridor import analyser_commande()
-from api import lister_parties(idul)
+import quoridor
+import api
+import argparse
+import requests
 
 if __name__ == "__main__":
-    analyser_commande()
-
-
-    
+    repo = vars(quoridor.analyser_commande())
+    idul = repo['idul']
+    x = api.initialiser_partie(idul)
+    y = x[1]
+    id_partie = x[0]
+    print(id_partie)
+    quitter = False
+    while not (quitter == True):
+        quoridor.afficher_damier_ascii(y)
+        type_coup = quoridor.demander_typecoup()
+        if type_coup == 'Q':
+            quitter = True
+            print('Au revoir')
+            break
+        position = quoridor.demander_position()
+        y = api.jouer_coup(id_partie, type_coup, position)
