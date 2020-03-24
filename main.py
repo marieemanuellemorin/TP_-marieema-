44c2e5253abcc1a6abbf19a756dbce311bf4aca7
marieemanuellemorin <62451810+marieemanuellemorin@users.noreply.github.com>
2020-03-24 12:43:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from quoridor import analyser_commande
+from quoridor import analyser_commande()
+from api import lister_parties(idul)
 
 if __name__ == "__main__":
     analyser_commande()
+
+
+    
