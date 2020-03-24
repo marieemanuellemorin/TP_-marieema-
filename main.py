45c67dacd19a918c6d4e39535815f1249fd9fac6
marieemanuellemorin <62451810+marieemanuellemorin@users.noreply.github.com>
2020-03-24 19:20:33 -0400

    revision

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import quoridor
 import api
-import argparse
-import requests
 
 if __name__ == "__main__":
     repo = vars(quoridor.analyser_commande())
@@ -11,12 +9,12 @@ if __name__ == "__main__":
     id_partie = x[0]
     print(id_partie)
     quitter = False
-    while not (quitter == True):
+    while quitter != True:
         quoridor.afficher_damier_ascii(y)
         type_coup = quoridor.demander_typecoup()
         if type_coup == 'Q':
             quitter = True
-            print('Au revoir')
+            print('Au revoir!')
             break
         position = quoridor.demander_position()
         y = api.jouer_coup(id_partie, type_coup, position)
