1da72f6a7fb2c7ceccc0d16b85ddd2cddaa7477b
marieemanuellemorin <62451810+marieemanuellemorin@users.noreply.github.com>
2020-03-25 12:04:02 -0400

    revision4

--- quoridor.py
+++ quoridor.py
@@ -3,14 +3,14 @@ import argparse
 
 def analyser_commande():
     # analyser la ligne de commande qui devrait être python main.py nom_du_joueur
-    parser = argparse.ArgumentParser(description = "Jeu Quoridor - phase 1")
+    parser = argparse.ArgumentParser(description="Jeu Quoridor - phase 1")
     parser.add_argument(
-        "idul", type = str, help='IDUL du joueur'
+        "idul", type=str, help='IDUL du joueur'
         )
 
     parser.add_argument(
-        '-l', "--lister", action = "store_true",
-         help="lister les identifiants de vos 20 dernières parties"
+        '-l', "--lister", action="store_true",
+        help="lister les identifiants de vos 20 dernières parties"
     )
     return parser.parse_args()
 
@@ -19,50 +19,50 @@ def afficher_damier_ascii(état_jeu):
     position_auto = état_jeu["joueurs"][1]["pos"]
     murh = état_jeu["murs"]["horizontaux"]
     murv = état_jeu["murs"]["verticaux"]
-    LHead1 ='   -----------------------------------'
-    LFoot1 ='--|-----------------------------------'
-    LFoot2 ='  | 1   2   3   4   5   6   7   8   9 '
-    VDebut = ' |'
-    HDebut = '  |'
-    Full = [[' . '] * 9, [' . '] * 9, [' . '] * 9, [' . '] * 9, [' . '] * 9, [' . '] * 9, [' . '] * 9, [' . '] * 9, [' . '] * 9]
-    Half = [['   '] * 9, ['   '] * 9, ['   '] * 9, ['   '] * 9, ['   '] * 9, ['   '] * 9, ['   '] * 9, ['   '] * 9, ['   '] * 9]
-    Full[position_idul[1]-1][position_idul[0]-1] = ' 1 '
+    lhead1 = '   -----------------------------------'
+    lfoot1 = '--|-----------------------------------'
+    lfoot2 = '  | 1   2   3   4   5   6   7   8   9 '
+    vdebut = ' |'
+    hdebut = '  |'
+    full = [[' . '] * 9, [' . '] * 9, [' . '] * 9, [' . '] * 9, [' . '] * 9, [' . '] * 9, [' . '] * 9, [' . '] * 9, [' . '] * 9]
+    half = [['   '] * 9, ['   '] * 9, ['   '] * 9, ['   '] * 9, ['   '] * 9, ['   '] * 9, ['   '] * 9, ['   '] * 9, ['   '] * 9]
+    full[position_idul[1]-1][position_idul[0]-1] = ' 1 '
     #AUTOMATE
-    Full[position_auto[1]-1][position_auto[0]-1] = ' 2 '
+    full[position_auto[1]-1][position_auto[0]-1] = ' 2 '
     #MUR VERTICAL
     l = len(murv)
     for i in range(l):
-        Full[murv[i][1]-1][murv[i][0]-1] = '| . '
-        Half[murv[i][1]][murv[i][0]-1] = '|   '
-        Full[murv[i][1]][murv[i][0]-1] = '| . '
+        full[murv[i][1]-1][murv[i][0]-1] = '| . '
+        half[murv[i][1]][murv[i][0]-1] = '|   '
+        full[murv[i][1]][murv[i][0]-1] = '| . '
     #MUR HORIZONTAL
     l = len(murh)
     for i in range(l):
-        if Half[murh[i][1]-1][murh[i][0]-1] == '|   ':
-            Half[murh[i][1]-1][murh[i][0]-1] = '|---'
+        if half[murh[i][1]-1][murh[i][0]-1] == '|   ':
+            half[murh[i][1]-1][murh[i][0]-1] = '|---'
         else:
-            Half[murh[i][1]-1][murh[i][0]-1] = '---'
-        Half[murh[i][1]-1][murh[i][0]] = '----'
+            half[murh[i][1]-1][murh[i][0]-1] = '---'
+        half[murh[i][1]-1][murh[i][0]] = '----'
     #AFFICHER TABLEAU
     print(f'Légende: 1={état_jeu["joueurs"][0]["nom"]}, 2={état_jeu["joueurs"][1]["nom"]}')
-    print(LHead1)
-    for Y in range(9, 0, -1):
-        Ligne_Full= f'{Y}' + VDebut
-        Ligne_Half = HDebut
-        for X in range(1, 10):
-            if (len(f'{Full[Y-1][X-1]}') == 3) and (X > 1):
-                Ligne_Full += ' '
-            Ligne_Full += f'{Full[Y-1][X-1]}'
-            if (len(f'{Half[Y-1][X-1]}') == 3) and (X > 1):
-                Ligne_Half += ' '
-            Ligne_Half += f'{Half[Y-1][X-1]}'
-        Ligne_Full += '|'
-        Ligne_Half += '|'
-        print(Ligne_Full)
+    print(lhead1)
+    for y in range(9, 0, -1):
+        lignefull = f'{y}' + vdebut
+        lignehalf = hdebut
+        for x in range(1, 10):
+            if (len(f'{full[y-1][x-1]}') == 3) and (x > 1):
+                lignefull += ' '
+            lignefull += f'{full[y-1][x-1]}'
+            if (len(f'{half[y-1][x-1]}') == 3) and (x > 1):
+                lignehalf += ' '
+            lignehalf += f'{half[y-1][x-1]}'
+        lignefull += '|'
+        lignehalf += '|'
+        print(lignefull)
         if Y > 1:
-            print(Ligne_Half)
-    print(LFoot1)
-    print(LFoot2)
+            print(lignehalf)
+    print(lfoot1)
+    print(lfoot2)
 
 def demander_typecoup():
     type_coup = input("Entrez type de coup:'Q': quitter, 'D': déplacer, 'MH': mur horizontal ou 'MV' mur vertical: ")
